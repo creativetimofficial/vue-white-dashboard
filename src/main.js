38681245b79f2166363fc3ca78b29fd8febb12fd
Maisam Afshar <maisam.afshar@gmail.com>
2022-12-15 10:02:31 +0430

    update version & date

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 /*!
 
 =========================================================
-* Vue White Dashboard - v1.0.1
+* Vue White Dashboard - v1.1.0
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/vue-white-dashboard
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/vue-white-dashboard/blob/master/LICENSE.md)
 
 * Coded by Creative Tim
@@ -17,22 +17,20 @@
 */
 import Vue from "vue";
 import VueRouter from "vue-router";
-import SocialSharing from "vue-social-sharing"
-import VueGitHubButtons from "vue-github-buttons"
-import "vue-github-buttons/dist/vue-github-buttons.css"
+import SocialSharing from "vue-social-sharing";
+import VueGitHubButtons from "vue-github-buttons";
+import "vue-github-buttons/dist/vue-github-buttons.css";
 import App from "./App.vue";
-import '@/assets/scss/white-dashboard.scss';
-import '@/assets/css/nucleo-icons.css';
+import "@/assets/scss/white-dashboard.scss";
+import "@/assets/css/nucleo-icons.css";
 import "@/assets/demo/demo.css";
-
 
 import GlobalComponents from "./globalComponents";
 import GlobalDirectives from "./globalDirectives";
 import RTLPlugin from "./RTLPlugin";
 import Notify from "@/components/NotificationPlugin";
-import i18n from "./i18n"
+import i18n from "./i18n";
 import SideBar from "@/components/SidebarPlugin";
-
 
 Vue.config.productionTip = false;
 
@@ -42,7 +40,7 @@ import routes from "./router";
 // configure router
 const router = new VueRouter({
   routes, // short for routes: routes
-  linkExactActiveClass: "active"
+  linkExactActiveClass: "active",
 });
 
 Vue.use(VueRouter);
@@ -57,5 +55,5 @@ Vue.use(Notify);
 new Vue({
   router,
   i18n,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
