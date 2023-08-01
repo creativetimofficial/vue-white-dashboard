8f2fec71163ecba751b6546c405bbe288d4f38e3
Jack Liao <xyz1357997531@gmail.com>
2023-08-02 02:33:34 +0800

    Add Table from Dashboard

--- src/pages/AD.vue
+++ src/pages/AD.vue
@@ -16,22 +16,61 @@
           </div>
         </card>
       </div>
-
-      <div class="col-12">
-        <card class="card-plain">
+    </div>
+    <div class="row">
+      <div class="col-lg-4">
+        <card type="chart" cardCol>
           <template slot="header">
-            <h4 class="card-title">Table on Plain Background</h4>
-            <p class="category">Here is a subtitle for this table</p>
+            <h5 class="card-category">{{ $t("dashboard.dailySales") }}</h5>
+            <h3 class="card-title">
+              <i class="tim-icons icon-delivery-fast text-info"></i> 3,500€
+            </h3>
           </template>
-          <div class="table-full-width table-responsive text-left">
-            <base-table
-              :title="table2.title"
-              :sub-title="table2.subTitle"
-              :data="table2.data"
-              :columns="table2.columns"
-            >
-            </base-table>
-          </div>
+          <bar-chart
+            class="chart-area"
+            chart-id="blue-bar-chart"
+            :chart-data="blueBarChart.chartData"
+            :gradient-stops="blueBarChart.gradientStops"
+            :extra-options="blueBarChart.extraOptions"
+          >
+          </bar-chart>
+        </card>
+      </div>
+      <div class="col-lg-4">
+        <card type="chart" cardCol>
+          <template slot="header">
+            <h5 class="card-category">{{ $t("dashboard.completedTasks") }}</h5>
+            <h3 class="card-title">
+              <i class="tim-icons icon-send text-success"></i> 12,100K
+            </h3>
+          </template>
+          <line-chart
+            class="chart-area"
+            chart-id="purple-line-chart"
+            :chart-data="purpleLineChart.chartData"
+            :gradient-stops="purpleLineChart.gradientStops"
+            :extra-options="purpleLineChart.extraOptions"
+          >
+          </line-chart>
+        </card>
+      </div>
+      <div class="col-lg-4" :class="{ 'text-right': isRTL }">
+        <card type="chart" cardCol>
+          <template slot="header">
+            <h5 class="card-category">{{ $t("dashboard.totalShipments") }}</h5>
+            <h3 class="card-title">
+              <i class="tim-icons icon-bell-55 text-primary"></i> 763,215
+            </h3>
+          </template>
+          <line-chart
+            class="chart-area"
+            chart-id="green-line-chart"
+            :chart-data="greenLineChart.chartData"
+            :gradient-colors="greenLineChart.gradientColors"
+            :gradient-stops="greenLineChart.gradientStops"
+            :extra-options="greenLineChart.extraOptions"
+          >
+          </line-chart>
         </card>
       </div>
     </div>
@@ -39,8 +78,11 @@
 </template>
 <script>
 import { Card } from "@/components/index";
-
+import LineChart from "@/components/Charts/LineChart";
+import BarChart from "@/components/Charts/BarChart";
+import * as chartConfigs from "@/components/Charts/config";
 import BaseTable from "@/components/BaseTable";
+import config from "@/config";
 
 import axios from "axios"; 
 
@@ -48,6 +90,8 @@ export default {
   components: {
     Card,
     BaseTable,
+    BarChart,
+    LineChart,
   },
   data() {
     return {
@@ -56,11 +100,81 @@ export default {
 	columns: [...table1Columns],
 	data: [],
      },
-     table2:{
-        title: "Table on Plain Background",
-        columns: [...tableColumns],
-        data: [...tableData],
-      },
+     blueBarChart: {
+       extraOptions: chartConfigs.barChartOptions,
+       chartData: {
+         labels: ["USA", "GER", "AUS", "UK", "RO", "BR"],
+         datasets: [
+           {
+             label: "Countries",
+             fill: true,
+             borderColor: config.colors.info,
+             borderWidth: 2,
+             borderDash: [],
+             borderDashOffset: 0.0,
+             data: [53, 20, 10, 80, 100, 45],
+           },
+         ],
+       },
+       gradientColors: config.colors.primaryGradient,
+       gradientStops: [1, 0.4, 0],
+     },
+     purpleLineChart: {
+       extraOptions: chartConfigs.purpleChartOptions,
+       chartData: {
+         labels: ["JUL", "AUG", "SEP", "OCT", "NOV"],
+         datasets: [
+           {
+             label: "My First dataset",
+             fill: true,
+             borderColor: config.colors.danger,
+             borderWidth: 2,
+             borderDash: [],
+             borderDashOffset: 0.0,
+             pointBackgroundColor: config.colors.danger,
+             pointBorderColor: "rgba(255,255,255,0)",
+             pointHoverBackgroundColor: config.colors.danger,
+             pointBorderWidth: 20,
+             pointHoverRadius: 4,
+             pointHoverBorderWidth: 15,
+             pointRadius: 4,
+             data: [90, 27, 60, 12, 80],
+           },
+         ],
+       },
+       gradientColors: [
+         "rgba(66,134,121,0.15)",
+         "rgba(66,134,121,0.0)",
+         "rgba(66,134,121,0)",
+       ],
+       gradientStops: [1, 0.4, 0],
+     },
+     greenLineChart: {
+       extraOptions: chartConfigs.greenChartOptions,
+       chartData: {
+         labels: ["JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
+         datasets: [
+           {
+             label: "Data",
+             fill: true,
+             borderColor: config.colors.primary,
+             borderWidth: 2,
+             borderDash: [],
+             borderDashOffset: 0.0,
+             pointBackgroundColor: config.colors.primary,
+             pointBorderColor: "rgba(255,255,255,0)",
+             pointHoverBackgroundColor: config.colors.primary,
+             pointBorderWidth: 20,
+             pointHoverRadius: 4,
+             pointHoverBorderWidth: 15,
+             pointRadius: 4,
+             data: [80, 100, 70, 80, 120, 80],
+           },
+         ],
+       },
+       gradientColors: config.colors.primaryGradient,
+       gradientStops: [1, 0.2, 0],
+     },
     };
   },
 methods : {
@@ -83,52 +197,7 @@ async mounted() {
 	console.log(this.table1.data);
 },
 };
-const table1Columns = ["DRB_AirDL","DRB_AirUL","DRB_RlcDL","DRB_RlcUL","Date","Handover","Total_Delay"];
-const tableColumns = ["id", "country", "city", "salary"];
-const tableData = [
-  {
-    id: 1,
-    name: "Dakota Rice",
-    salary: 36.738,
-    country: "Niger",
-    city: "Oud-Turnhout",
-  },
-  {
-    id: 2,
-    name: "Minerva Hooper",
-    salary: "$23,789",
-    country: "Curaçao",
-    city: "Sinaai-Waas",
-  },
-  {
-    id: 3,
-    name: "Sage Rodriguez",
-    salary: "$56,142",
-    country: "Netherlands",
-    city: "Baileux",
-  },
-  {
-    id: 4,
-    name: "Philip Chaney",
-    salary: "$38,735",
-    country: "Korea, South",
-    city: "Overland Park",
-  },
-  {
-    id: 5,
-    name: "Doris Greene",
-    salary: "$63,542",
-    country: "Malawi",
-    city: "Feldkirchen in Kärnten",
-  },
-  {
-    id: 6,
-    name: "Mason Porter",
-    salary: "$98,615",
-    country: "Chile",
-    city: "Gloucester",
-  },
-];
+const table1Columns = ["Date","DRB_AirDL","DRB_AirUL","DRB_RlcDL","DRB_RlcUL","Handover","Total_Delay"];
 
 
 </script>
